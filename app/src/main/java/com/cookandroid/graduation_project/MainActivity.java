166c2e8fe2b29bee18397b29d772ac6aa892334c
euncm <silver_1102@naver.com>
2021-10-14 03:40:51 +0900

    카톡 프로필사진 받아와서 띄우는 기능 추가 로그인시 홈 화면으로 넘어가며 정보 넘겨주는 기능 추가 (테스트 필요!)

--- app/src/main/java/com/cookandroid/graduation_project/MainActivity.java
+++ app/src/main/java/com/cookandroid/graduation_project/MainActivity.java
@@ -1,23 +1,29 @@
 package com.cookandroid.graduation_project;
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.kakao.sdk.auth.model.OAuthToken;
-import com.kakao.sdk.user.UserApi;
 import com.kakao.sdk.user.UserApiClient;
 import com.kakao.sdk.user.model.User;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
 import kotlin.jvm.functions.Function2;
+import kotlin.jvm.functions.Function3;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
-    private View loginButton, logoutButton;
+    private View loginButton, logoutButton, profileImage;
     private TextView nickName;
 
     @Override
@@ -28,7 +34,10 @@ public class MainActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.login);
         logoutButton = findViewById(R.id.logout);
         nickName = findViewById(R.id.nickname);
+        profileImage = (ImageView)findViewById(R.id.profile);
+        profileImage.setBackgroundResource(R.drawable.custom_button_gray);
 
+        nickName.setText("로그인이 필요합니다.");
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,10 +46,11 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
                             if(oAuthToken != null){
-                                //TBD
+                                Log.i(TAG, "로그인 성공");
+                                loginSucceed();
                             }
                             if(throwable != null){
-                                //TBD
+                                Log.e(TAG, "로그인 실패");
                             }
                             updateKakaoLoginUi();
                             return null;
@@ -48,23 +58,23 @@ public class MainActivity extends AppCompatActivity {
                     });
                 }
                 else{
-                    UserApiClient.getInstance().loginWithKakaoAccount(MainActivity.this, new Function2<OAuthToken, Throwable, Unit>() {
+                    Log.e(TAG, "카카오톡 설치가 필요합니다.");
+                    /*UserApiClient.getInstance().loginWithKakaoAccount(MainActivity.this, new Function2<OAuthToken, Throwable, Unit>() {
                         @Override
                         public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
                             if(oAuthToken != null){
                                 //TBD
                             }
                             if(throwable != null){
-                                //TBD
+                                Log.e(TAG, "사용자 정보 요청 실패");
                             }
                             updateKakaoLoginUi();
                             return null;
                         }
-                    });
+                    });*/
                 }
             }
         });
-
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -79,21 +89,34 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     private void updateKakaoLoginUi(){
         UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
             @Override
             public Unit invoke(User user, Throwable throwable) {
                 if(user != null) {
-
                     nickName.setText(user.getKakaoAccount().getProfile().getNickname());
-
+                    Glide.with(getApplicationContext()).load(user.getKakaoAccount().getProfile().getProfileImageUrl()).into((ImageView) profileImage);
                     loginButton.setVisibility(View.GONE);
                     logoutButton.setVisibility(View.VISIBLE);
                 } else {
-
                     loginButton.setVisibility(View.VISIBLE);
                     logoutButton.setVisibility(View.GONE);
                 }
+                return null;
+            }
+        });
+    }
+
+    private void loginSucceed(){
+        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+            @Override
+            public Unit invoke(User user, Throwable throwable) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                intent.putExtra("email",user.getKakaoAccount().getEmail());
+                intent.putExtra("nickname",user.getKakaoAccount().getProfile().getNickname());
+                intent.putExtra("profileImageURL",user.getKakaoAccount().getProfile().getProfileImageUrl());
+                startActivity(intent);
                 return null;
             }
         });
